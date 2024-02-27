86a97d5ea5b4858de516225359f951a296c6d8e3
Pampukin <20010903yuusuke@gmail.com>
2024-02-28 02:36:56 +0900

    ボールのトリガーでの実装

--- Assets/Scripts/Ball/BallMoverTrigger.cs
+++ Assets/Scripts/Ball/BallMoverTrigger.cs
@@ -1,47 +1,52 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallMover : MonoBehaviour, IHit
+public class BallMoverTrigger : MonoBehaviour, IHit
 {
     private Rigidbody2D _rb;
+
+    private Transform _myTrans;
 
     /// <summary>
     /// ボールの速さ
     /// </summary>
     private Vector3 _moveVelocity
     {
-        get
-        {
-            return _moveSpeed * _reflectVector.normalized;
-        }
+        get { return _moveSpeed * _reflectVector.normalized; }
     }
-    
+
     /// <summary>
     /// 速度
     /// </summary>
     [SerializeField]
     private float _moveSpeed = 5;
-    
+
     /// <summary>
     /// 反射する方向
     /// </summary>
     private Vector3 _reflectVector;
-    
+
     private Vector3 _hitPos;
-    
-    
+
+
     private bool _hasHit = true;
 
     [SerializeField]
     private Crash _crash;
 
     private List<GameObject> _crashObjects = new List<GameObject>();
-    
-    private Collision2D _col;
+
+    private Collider2D _col;
+
+    private Ray2D _hitRay = new Ray2D();
+    private RaycastHit2D _hit;
 
     private void Awake()
     {
         _rb = this.GetComponent<Rigidbody2D>();
+        _myTrans = this.GetComponent<Transform>();
         _OnlyDebug();
     }
 
@@ -64,39 +69,51 @@ public class BallMover : MonoBehaviour, IHit
         switch (_col.gameObject.tag)
         {
             case "Ball":
-                _reflectVector =  this.transform.position - _hitPos;
+                _reflectVector = this.transform.position - _col.transform.position;
                 break;
-            
+
             case "Racket":
                 var racket = _col.gameObject.GetComponent<Racket>();
                 if (racket == null) return;
 
                 _SetReflectVector(_hitPos.x - racket.transform.position.x, racket.Height / 2, 0);
                 break;
-            
+
             default:
-                if (Vector2.Dot(_col.contacts[0].normal.normalized, _moveVelocity) < 0)
+                _SetHitRay(transform.position,_hitPos.normalized);
+                _hit = Physics2D.Raycast(_hitRay.origin, _hitRay.direction, 1f); //Raycastを生成
+                if (Vector2.Dot(_hit.normal, _moveVelocity) < 0)
                 {
-                    _reflectVector = Vector2.Reflect(_moveVelocity, _col.contacts[0].normal.normalized);
+                    _reflectVector = Vector2.Reflect(_moveVelocity, _hit.normal);
+                }
+                if (_hit.collider)
+                {
+                    
                 }
                 break;
         }
     }
-    
+
     private void _SetReflectVector(float x, float y, float z)
     {
         _reflectVector.x = x;
         _reflectVector.y = y;
         _reflectVector.z = z;
     }
-    
+
+    private void _SetHitRay(Vector2 origin, Vector2 direction)
+    {
+        _hitRay.origin = origin;
+        _hitRay.direction = direction;
+    }
+
     /// <summary>
     /// 接触点の生成
     /// </summary>
     /// <param name="col"></param>
-    private void _SetCrash(Collision2D col)
+    private void _SetCrash(Collider2D col)
     {
-        var crash = Instantiate(_crash, col.contacts[0].point, Quaternion.identity, this.transform);
+        var crash = Instantiate(_crash, col.ClosestPoint(_myTrans.position), Quaternion.identity, this.transform);
         _crashObjects.Add(crash.gameObject);
         crash.MyObject = this.gameObject;
         crash.OtherObject = col.gameObject;
@@ -106,9 +123,9 @@ public class BallMover : MonoBehaviour, IHit
     /// 接触点の更新
     /// </summary>
     /// <param name="col"></param>
-    private void _RenewCrash(Collision2D col)
+    private void _RenewCrash(Collider2D col)
     {
-        _crashObjects[^1].transform.position = col.contacts[0].point;
+        _crashObjects[^1].transform.position = col.ClosestPoint(_myTrans.position);
     }
 
     /// <summary>
@@ -120,37 +137,40 @@ public class BallMover : MonoBehaviour, IHit
         {
             Destroy(crashObject);
         }
+
         _crashObjects.Clear();
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         _col = col;
-        _hitPos = col.contacts[0].point;
+        _hitPos = col.ClosestPoint(this.transform.position);
+        Hit();
         
         _SetCrash(col);
     }
 
-    private void OnCollisionStay2D(Collision2D col)
+    private void OnTriggerStay2D(Collider2D col)
     {
         _col = col;
-        _hitPos = col.contacts[0].point;
+        _hitPos = col.ClosestPoint(_myTrans.position);
+        Hit();
         
         if (_crashObjects.Count != 0)
         {
             _RenewCrash(col);
-
-            Hit();
         }
     }
 
-    private void OnCollisionExit2D(Collision2D col)
+    private void OnTriggerExit2D(Collider2D col)
     {
         _col = col;
+        
         if (_crashObjects.Count != 0)
         {
             _ResetCrash();
         }
         _hitPos = Vector3.zero;
     }
+    
 }
